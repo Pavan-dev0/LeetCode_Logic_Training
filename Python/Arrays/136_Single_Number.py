d63742d42edff0666fea02b5689774709f4fb8f5
Pavan-dev0 <altairpheonix@gmail.com>
2026-03-03 23:01:51 +0530

    136_sinlge_number_leetcode_python

---
+++ Python/Arrays/136_Single_Number.py
@@ -0,0 +1,8 @@
+from typing import List 
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        index=0
+        for num in nums:
+            index^=num
+        return index
